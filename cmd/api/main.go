db4809563ff757b49823a5524c68e2fbb5f630a3
Jacob Slunga <jacobslunga@Jacobs-MacBook-Air.local>
2023-12-26 17:28:20 +0100

    added handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/go-chi/chi"
-  "github.com/jacobslunga/goapi/internal/handlers"
-  log "github.com/sirupsen/logrus"
+	"github.com/go-chi/chi"
+	"github.com/jacobslunga/goapi/internal/handlers"
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+  log.SetReportCaller(true)
+  var r *chi.Router = chi.NewRouter()
+  handlers.Handler(r)
 
+  fmt.Println("Starting our Go API")
+
+  err := http.ListenAndServe("localhost:8000", r)
+
+  if err != nil {
+    log.Fatal(err)
+  }
 }
